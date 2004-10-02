f457164ebca9d2da58edc23814582b6c1aed7f9b
Greg Beaver <cellog@php.net>
2004-10-02 05:47:18 +0000

    add pre-install script task "preinstallscript" fix copy/paste errors in documentation in post-install

--- PEAR/Task/Preinstallscript.php
+++ PEAR/Task/Preinstallscript.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Implements the postinstallscript file task
+ * Implements the preinstallscript file task
  * @package PEAR
  * @author Greg Beaver <cellog@php.net>
  */
-class PEAR_Task_Postinstallscript extends PEAR_Task_Common
+class PEAR_Task_Preinstallscript extends PEAR_Task_Common
 {
-    var $type = 'postinstall-multiple';
+    var $type = 'multiple';
     var $_class;
     var $_obj;
 
@@ -14,7 +14,7 @@ class PEAR_Task_Postinstallscript extends PEAR_Task_Common
      * Validate the raw xml at parsing-time.
      *
      * This also attempts to validate the script to make sure it meets the criteria
-     * for a post-install script
+     * for a pre-install script
      * @param PEAR_PackageFile_v2
      * @param array
      * @param PEAR_Config
@@ -24,35 +24,35 @@ class PEAR_Task_Postinstallscript extends PEAR_Task_Common
     function validateXml($pkg, $xml, &$config, $fileXml)
     {
         if ($fileXml['attribs']['role'] != 'php') {
-            return array(PEAR_TASK_ERROR_INVALID, 'Post-install script "' .
+            return array(PEAR_TASK_ERROR_INVALID, 'Pre-install script "' .
             $fileXml['attribs']['name'] . '" must be role="php"');
         }
         PEAR::pushErrorHandling(PEAR_ERROR_RETURN);
         $file = $pkg->getFileContents($fileXml['attribs']['name']);
         if (PEAR::isError($file)) {
-            return array(PEAR_TASK_ERROR_INVALID, 'Post-install script "' .
+            return array(PEAR_TASK_ERROR_INVALID, 'Pre-install script "' .
                 $fileXml['attribs']['name'] . '"is not valid: ' .
                 $file->getMessage());
         } else {
             $analysis = $pkg->analyzeSourceCode($file, true);
             if (PEAR::isError($analysis)) {
-                return array(PEAR_TASK_ERROR_INVALID, 'Analysis of post-install script "' .
+                return array(PEAR_TASK_ERROR_INVALID, 'Analysis of pre-install script "' .
                     $fileXml['attribs']['name'] . '"failed');
             }
             if (count($analysis['declared_classes']) != 1) {
-                return array(PEAR_TASK_ERROR_INVALID, 'Post-install script "' .
+                return array(PEAR_TASK_ERROR_INVALID, 'Pre-install script "' .
                     $fileXml['attribs']['name'] . '"must declare exactly 1 class');
             }
             $class = $analysis['declared_classes'][0];
             if ($class != str_replace(array('/', '.php'), array('_', ''),
                   $fileXml['attribs']['name']) . '_postinstall') {
-                return array(PEAR_TASK_ERROR_INVALID, 'Post-install script "' .
+                return array(PEAR_TASK_ERROR_INVALID, 'Pre-install script "' .
                     $fileXml['attribs']['name'] . '" class "' . $class . '" must be named "' .
                     str_replace(array('/', '.php'), array('_', ''),
-                    $fileXml['attribs']['name']) . '_postinstall"');
+                    $fileXml['attribs']['name']) . '_preinstall"');
             }
             if (!isset($analysis['declared_methods'][$class])) {
-                return array(PEAR_TASK_ERROR_INVALID, 'Post-install script "' .
+                return array(PEAR_TASK_ERROR_INVALID, 'Pre-install script "' .
                     $fileXml['attribs']['name'] . '" must declare methods init() and run()');
             }
             $methods = array('init' => 0, 'run' => 1);
@@ -62,7 +62,7 @@ class PEAR_Task_Postinstallscript extends PEAR_Task_Common
                 }
             }
             if (count($methods)) {
-                return array(PEAR_TASK_ERROR_INVALID, 'Post-install script "' .
+                return array(PEAR_TASK_ERROR_INVALID, 'Pre-install script "' .
                     $fileXml['attribs']['name'] . '"must declare methods init() and run()');
             }
         }
@@ -79,7 +79,7 @@ class PEAR_Task_Postinstallscript extends PEAR_Task_Common
     {
         $this->_class = str_replace('/', '_', $fileattribs['name']);
         $this->_filename = $fileattribs['name'];
-        $this->_class = str_replace ('.php', '', $this->_class) . '_postinstall';
+        $this->_class = str_replace ('.php', '', $this->_class) . '_preinstall';
     }
 
     /**
@@ -93,31 +93,31 @@ class PEAR_Task_Postinstallscript extends PEAR_Task_Common
     {
         $orig = $contents;
         $contents = str_replace(array('<?php', '?>'), array('', ''), $contents);
-        $this->installer->log(0, 'Including external post-installation script "' .
+        $this->installer->log(0, 'Including external pre-installation script "' .
             $dest . '" - any fatal errors are in this script');
         eval($contents);
         if (class_exists($this->_class)) {
             $this->installer->log(0, 'Inclusion succeeded');
         } else {
-            return $this->throwError('init of post-install script class "' . $this->_class
+            return $this->throwError('init of pre-install script class "' . $this->_class
                 . '" failed');
         }
         $this->_obj = new $this->_class;
-        $this->installer->log(1, 'running post-install script "' . $this->_class . '->init()"');
+        $this->installer->log(1, 'running pre-install script "' . $this->_class . '->init()"');
         PEAR::pushErrorHandling(PEAR_ERROR_RETURN);
         $res = $this->_obj->init($this->config, $this->installer->ui);
         PEAR::popErrorHandling();
         if ($res) {
             $this->installer->log(0, 'init succeeded');
         } else {
-            return $this->throwError('init of post-install script "' . $this->_class .
+            return $this->throwError('init of pre-install script "' . $this->_class .
                 '->init()" failed');
         }
         return $orig; // unchanged
     }
 
     /**
-     * Run the post-installation script
+     * Run the pre-installation script
      * @param array an array of tasks
      * @access protected
      * @static
