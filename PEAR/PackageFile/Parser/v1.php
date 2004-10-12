c331a560325085315c49e3c7bea79bf570700b43
Greg Beaver <cellog@php.net>
2004-10-12 19:42:43 +0000

    add header

--- PEAR/PackageFile/Parser/v1.php
+++ PEAR/PackageFile/Parser/v1.php
@@ -1,7 +1,29 @@
 <?php
+//
+// +----------------------------------------------------------------------+
+// | PHP Version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.php.net/license/3_0.txt.                                  |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Author: Greg Beaver <cellog@php.net>                                 |
+// |                                                                      |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
 require_once 'PEAR/PackageFile/v1.php';
 /**
  * Parser for package.xml version 1.0
+ * @author Greg Beaver <cellog@php.net>
+ * @package PEAR
  */
 class PEAR_PackageFile_Parser_v1
 {
