909f1ff2531efcd73870319979fb866bf9285629
Greg Beaver <cellog@php.net>
2004-10-28 22:25:59 +0000

    add standard thing to top of file

--- PEAR/PackageFile/v2/Validator.php
+++ PEAR/PackageFile/v2/Validator.php
@@ -1,7 +1,27 @@
 <?php
+//
+// +----------------------------------------------------------------------+
+// | PHP Version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.php.net/license/3_0.txt.                                  |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Author: Greg Beaver <cellog@php.net>                                 |
+// |                                                                      |
+// +----------------------------------------------------------------------+
+//
+// $Id$
 /**
  * Private validation class used by PEAR_PackageFile_v2 - do not use directly, its
  * sole purpose is to split up the PEAR/PackageFile/v2.php file to make it smaller
+ * @author Greg Beaver <cellog@php.net>
  * @access private
  */
 class PEAR_PackageFile_v2_Validator
