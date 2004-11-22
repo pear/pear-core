a8a6f67dd3502f5f93ff4789c4b4e256d39d8927
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-11-22 05:08:22 +0000

    new test
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@173191 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/PEAR_Installer_Role/roles/Phpf.php
@@ -0,0 +1,31 @@
+<?php
+class PEAR_Installer_Role_Phpf extends PEAR_Installer_Role_Common
+{
+    var $_setup =
+        array(
+            'releasetypes' => array('php'),
+            'installable' => true,
+            'locationconfig' => 'php_dir',
+            'honorsbaseinstall' => true,
+            'phpfile' => true,
+            'executable' => false,
+            'phpextension' => false,
+        );
+    function getInfo()
+    {
+        return array(
+            'releasetypes' => array('php'),
+            'installable' => true,
+            'locationconfig' => 'php_dir',
+            'honorsbaseinstall' => true,
+            'phpfile' => true,
+            'executable' => false,
+            'phpextension' => false,
+        );
+    }
+
+    function setup(&$installer, $pkg, $atts, $file)
+    {
+    }
+}
+?>
