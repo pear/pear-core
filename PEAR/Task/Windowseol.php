10a06cdbd026a47ed043f5a3a1c686600301e107
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-12-31 18:33:22 +0000

    add eol tasks
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@176130 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ PEAR/Task/Windowseol.php
@@ -0,0 +1,53 @@
+<?php
+require_once 'PEAR/Task/Common.php';
+/**
+ * Implements the windowseol file task
+ * @package PEAR
+ * @author Greg Beaver <cellog@php.net>
+ */
+class PEAR_Task_Windowseol extends PEAR_Task_Common
+{
+    var $type = 'simple';
+    var $phase = PEAR_TASK_PACKAGE;
+    var $_replacements;
+
+    /**
+     * Validate the raw xml at parsing-time.
+     * @param PEAR_PackageFile_v2
+     * @param array raw, parsed xml
+     * @param PEAR_Config
+     * @static
+     */
+    function validateXml($pkg, $xml, &$config, $fileXml)
+    {
+        if ($xml != array()) {
+            return array(PEAR_TASK_ERROR_INVALID);
+        }
+    }
+
+    /**
+     * Initialize a task instance with the parameters
+     * @param array raw, parsed xml
+     * @param unused
+     */
+    function init($xml, $attribs)
+    {
+    }
+
+    /**
+     * Replace all line endings with windows line endings
+     *
+     * See validateXml() source for the complete list of allowed fields
+     * @param PEAR_PackageFile_v1|PEAR_PackageFile_v2
+     * @param string file contents
+     * @param string the eventual final file location (informational only)
+     * @return string|false|PEAR_Error false to skip this file, PEAR_Error to fail
+     *         (use $this->throwError), otherwise return the new contents
+     */
+    function startSession($pkg, $contents, $dest)
+    {
+        $this->installer->log(3, "replacing all line endings with \\r\\n in $dest");
+        return preg_replace("/\r\n|\n\r|\r|\n/", "\r\n", $contents);
+    }
+}
+?>
