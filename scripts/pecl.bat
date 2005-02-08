0c7379c6da49004bd9d20937bc37288395d6da19
Greg Beaver <cellog@php.net>
2005-02-08 15:52:13 +0000

    update mod date

--- scripts/pecl.bat
+++ scripts/pecl.bat
@@ -16,7 +16,7 @@ REM ----------------------------------------------------------------------
 REM  Authors:     Alexander Merz (alexmerz@php.net)
 REM ----------------------------------------------------------------------
 REM
-REM  Last updated 12/29/2004 ($Id$ is not replaced if the file is binary)
+REM  Last updated 02/08/2004 ($Id$ is not replaced if the file is binary)
 
 REM change this lines to match the paths of your system
 REM -------------------
