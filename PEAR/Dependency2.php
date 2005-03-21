02c42d6126c570caa9bd69c6ff98b7328ae0b3eb
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-03-21 01:14:50 +0000

    use existing replacement
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@182526 c90b9560-bf6c-de11-be94-00142212c4b1

--- PEAR/Dependency2.php
+++ PEAR/Dependency2.php
@@ -37,7 +37,7 @@ require_once 'PEAR/Validate.php';
  * @author     Greg Beaver <cellog@php.net>
  * @copyright  1997-2005 The PHP Group
  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    Release: @package_version@
+ * @version    Release: @PEAR-VER@
  * @link       http://pear.php.net/package/PEAR
  * @since      Class available since Release 1.4.0a1
  */
