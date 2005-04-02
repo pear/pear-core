192ecfeb87e414392117f1fc5d1b9f95cc50fcc0
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-04-02 23:38:11 +0000

    fix Bug #3990: PEAR_Error PEAR_EXCEPTION broken
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@183442 c90b9560-bf6c-de11-be94-00142212c4b1

--- PEAR.php
+++ PEAR.php
@@ -888,7 +888,7 @@ class PEAR_Error
         }
         if ($this->mode & PEAR_ERROR_EXCEPTION) {
             trigger_error("PEAR_ERROR_EXCEPTION is obsolete, use class PEAR_ErrorStack for exceptions", E_USER_WARNING);
-            eval('$e = new Exception($this->message, $this->code);$e->PEAR_Error = $this;throw($e);');
+            eval('$e = new Exception($this->message, $this->code);throw($e);');
         }
     }
 
