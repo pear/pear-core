0256bf92ad24c93a419565ea798b432150860131
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-23 04:21:50 +0000

    fix Autoload.php to allow multiple inclusion (although this is discouraged for obvious reasons)
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@227943 c90b9560-bf6c-de11-be94-00142212c4b1

--- PEAR2/Autoload.php
+++ PEAR2/Autoload.php
@@ -1,6 +1,9 @@
 <?php
-if (function_exists('PEAR2_Autoload')) return;
-function PEAR2_Autoload($class)
-{
-    require str_replace('_', '/', $class) . '.php';
+if (function_exists('PEAR2_Autoload')) {
+    return;
+} else {
+    function PEAR2_Autoload($class)
+    {
+        require str_replace('_', '/', $class) . '.php';
+    }
 }
