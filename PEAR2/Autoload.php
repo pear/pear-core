a5981de7bdb70a15fc35d069552778ca0c1c53ea
Gwynne Raskind <gwynne@php.net>
2007-01-24 00:16:58 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_5_0'.

--- PEAR2/Autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (function_exists('PEAR2_Autoload')) {
-    return;
-} else {
-    function PEAR2_Autoload($class)
-    {
-        require str_replace('_', '/', $class) . '.php';
-    }
-}
--- PEAR2/Dependency/Exception.php
+++
@@ -1,2 +0,0 @@
-<?php
-class PEAR2_Dependency_Exception extends PEAR2_Exception {}
