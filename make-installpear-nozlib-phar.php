a13aa842e83a74e7ac16a6ad412e6f0ccc997297
Greg Beaver <cellog@php.net>
2007-05-29 23:26:13 +0000

    fix install-pear-nozlib creation

--- make-installpear-nozlib-phar.php
+++ make-installpear-nozlib-phar.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * install-pear-nozlib.phar creator.  Requires PHP_Archive version 0.10.0 or newer
+ * install-pear-nozlib.phar creator.  Requires PHP_Archive version 0.11.0 or newer
  *
  * PHP version 5.1+
  *
@@ -29,6 +29,12 @@
  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
  * @version    CVS: $Id$
  */
+
+function replaceVersion($contents, $path)
+{
+    return str_replace(array('@PEAR-VER@', '@package_version@'), $GLOBALS['pearver'], $contents);
+}
+
 $peardir = dirname(__FILE__);
 
 $dp = @opendir(dirname(__FILE__) . '/PEAR/go-pear-tarballs');
@@ -71,6 +77,7 @@ $pf = $pkg->fromPackageFile($peardir . DIRECTORY_SEPARATOR . 'package2.xml', PEA
 $pearver = $pf->getVersion();
 
 $creator = new PHP_Archive_Creator('index.php', 'install-pear-nozlib.phar'); // no compression
+$creator->useDefaultFrontController('PEAR.php');
 $creator->useSHA1Signature();
 $install_files = '$install_files = array(';
 foreach ($packages as $name => $package) {
@@ -92,222 +99,44 @@ $commandcontents = str_replace(
     ), $commandcontents);
 $creator->addString($commandcontents, 'index.php');
 
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'Command.php');
+$commandcontents = file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . '/PEAR/Frontend.php');
 $commandcontents = str_replace(
     array(
-        "require_once '",
-        "include_once '",
+        "\$file = str_replace('_', '/', \$uiclass) . '.php';"
     ),
     array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/Command.php');
-
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'PackageFile' . DIRECTORY_SEPARATOR . 'v2.php');
-$commandcontents = str_replace(
-    array(
-        "require_once '",
-        "include_once '",
-        'if (file_exists($path . "/PEAR/Task/$task.php")) {',
-        "include_once \"PEAR/Task/\$task.php",
-    ),
-    array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        'if (true) {', // we're self-contained, so this should work
-        "include_once \"phar://install-pear-nozlib.phar/PEAR/Task/\$task.php",
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/PackageFile/v2.php');
-
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'Frontend.php');
-$commandcontents = str_replace(
-    array(
-        "include_once ",
-        'PEAR_Frontend::isIncludeable($file)',
-    ),
-    array(
-        "include_once 'phar://install-pear-nozlib.phar/' . ",
-        'true',
-    ),
-    $commandcontents);
+        "\$file = 'phar://install-pear-nozlib.phar/' . str_replace('_', '/', \$uiclass) . '.php';"
+    ), $commandcontents);
+$commandcontents = replaceVersion($commandcontents, '');
 $creator->addString($commandcontents, 'PEAR/Frontend.php');
 
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'Downloader' . DIRECTORY_SEPARATOR . 'Package.php');
+$commandcontents = file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . '/PEAR/PackageFile/v2.php');
 $commandcontents = str_replace(
     array(
-        "require_once '",
-        "include_once '",
-        "@PEAR-VER@",
+        '$fp = @fopen("PEAR/Task/$taskfile.php", \'r\', true);',
     ),
     array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        $pearver,
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/Downloader/Package.php');
+        '$fp = @fopen("phar://install-pear-nozlib.phar/PEAR/Task/$taskfile.php", \'r\', true);'
+    ), $commandcontents);
+$commandcontents = replaceVersion($commandcontents, '');
+$commandcontents = $creator->tokenMagicRequire($commandcontents, 'a.php');
+$creator->addString($commandcontents, 'PEAR/PackageFile/v2.php');
 
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'PackageFile' . DIRECTORY_SEPARATOR . 'v2' .
-    DIRECTORY_SEPARATOR . 'Validator.php');
-$commandcontents = str_replace(
-    array(
-        "require_once '",
-        "include_once '",
-        "@package_version@",
-    ),
-    array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        $pearver,
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/PackageFile/v2/Validator.php');
+$creator->addMagicRequireCallback(array($creator, 'limitedSmartMagicRequire'));
+$creator->addMagicRequireCallback('replaceVersion');
+$creator->addFile($peardir . '/PEAR/Command.php', 'PEAR/Command.php');
 
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'PackageFile' . DIRECTORY_SEPARATOR . 'v2' .
-    DIRECTORY_SEPARATOR . 'rw.php');
-$commandcontents = str_replace(
+$creator->clearMagicRequire();
+$creator->addMagicRequireCallback(array($creator, 'tokenMagicRequire'));
+$creator->addMagicRequireCallback('replaceVersion');
+$creator->addDir($peardir . DIRECTORY_SEPARATOR . 'PEAR', array(),
     array(
-        "require_once '",
-        "include_once '",
-        "@package_version@",
-    ),
-    array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        $pearver,
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/PackageFile/v2/rw.php');
-
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'PackageFile' . DIRECTORY_SEPARATOR . 'Generator' .
-    DIRECTORY_SEPARATOR . 'v1.php');
-$commandcontents = str_replace(
-    array(
-        "require_once '",
-        "include_once '",
-        "@PEAR-VER@",
-    ),
-    array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        $pearver,
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/PackageFile/Generator/v1.php');
-
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'PackageFile' . DIRECTORY_SEPARATOR . 'Generator' .
-    DIRECTORY_SEPARATOR . 'v2.php');
-$commandcontents = str_replace(
-    array(
-        "require_once '",
-        "include_once '",
-        "@PEAR-VER@",
-    ),
-    array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        $pearver,
-    ),
-    $commandcontents);
-
-$creator->addString($commandcontents, 'PEAR/PackageFile/Generator/v2.php');
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'Installer' .
-    DIRECTORY_SEPARATOR . 'Role.php');
-$commandcontents = str_replace(
-    array(
-        "require_once '",
-        "include_once '",
-        "require_once str_replace('_', '/', \$a)",
-        "@PEAR-VER@",
-    ),
-    array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        "require_once 'phar://install-pear-nozlib.phar/' . str_replace('_', '/', \$a)",
-        $pearver,
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/Installer/Role.php');
-
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'Dependency2.php');
-$commandcontents = str_replace(
-    array(
-        "require_once '",
-        "include_once '",
-        "@PEAR-VER@",
-    ),
-    array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        $pearver,
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/Dependency2.php');
-
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'OS' .
-    DIRECTORY_SEPARATOR . 'Guess.php');
-$commandcontents = str_replace(
-    array(
-        "include_once \"",
-        "@package_version@",
-    ),
-    array(
-        "include_once \"phar://install-pear-nozlib.phar/",
-        $pearver,
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'OS/Guess.php');
-
-$commandcontents = file_get_contents($peardir . DIRECTORY_SEPARATOR . 'PEAR' .
-    DIRECTORY_SEPARATOR . 'PackageFile.php');
-$commandcontents = str_replace(
-    array(
-        "require_once '",
-        "include_once '",
-        "@PEAR-VER@",
-    ),
-    array(
-        "require_once 'phar://install-pear-nozlib.phar/",
-        "include_once 'phar://install-pear-nozlib.phar/",
-        $pearver,
-    ),
-    $commandcontents);
-$creator->addString($commandcontents, 'PEAR/PackageFile.php');
-
-$creator->addMagicRequireCallback(array($creator, 'simpleMagicRequire'));
-$creator->addDir($peardir, array('tests/',
-    'scripts/',
-    'go-pear-phar.php',
-    '*OS/Guess.php',
-    '*PEAR/Command.php',
-    '*PEAR/Dependency2.php',
-    '*PEAR/PackageFile/Generator/v1.php',
-    '*PEAR/PackageFile/Generator/v2.php',
-    '*PEAR/PackageFile/v2.php',
-    '*PEAR/PackageFile.php',
-    '*PEAR/Downloader/Package.php',
-    '*PEAR/Start.php',
-    '*PEAR/Start/CLI.php',
-    '*PEAR/Installer/Role.php',
-    '*PEAR/Frontend.php'),
-    array(
-        '*Structures/Graph.php',
-        '*Structures/Graph/Node.php',
-        '*Structures/Graph/Manipulator/AcyclicTest.php',
-        '*Structures/Graph/Manipulator/TopologicalSorter.php',
+        '*PEAR/Dependency2.php',
+        '*PEAR/PackageFile/Generator/v1.php',
+        '*PEAR/PackageFile/Generator/v2.php',
+        '*PEAR/PackageFile/v2/Validator.php',
+        '*PEAR/Downloader/Package.php',
+        '*PEAR/Installer/Role.php',
         '*PEAR/ChannelFile/Parser.php',
         '*PEAR/Command/Install.xml',
         '*PEAR/Command/Install.php',
@@ -324,8 +153,8 @@ $creator->addDir($peardir, array('tests/',
         '*PEAR/Installer/Role/Php.xml',
         '*PEAR/Installer/Role/Script.xml',
         '*PEAR/Installer/Role/Test.xml',
+        '*PEAR/PackageFile.php',
         '*PEAR/PackageFile/v1.php',
-        '*PEAR/PackageFile/v2.php',
         '*PEAR/PackageFile/Parser/v1.php',
         '*PEAR/PackageFile/Parser/v2.php',
         '*PEAR/PackageFile/Generator/v1.php',
@@ -342,7 +171,6 @@ $creator->addDir($peardir, array('tests/',
         '*PEAR/Task/Unixeol/rw.php',
         '*PEAR/Validator/PECL.php',
         '*PEAR/ChannelFile.php',
-        '*PEAR/Command.php',
         '*PEAR/Command/Common.php',
         '*PEAR/Common.php',
         '*PEAR/Config.php',
@@ -350,16 +178,22 @@ $creator->addDir($peardir, array('tests/',
         '*PEAR/DependencyDB.php',
         '*PEAR/Downloader.php',
         '*PEAR/ErrorStack.php',
-        '*PEAR/Frontend.php',
         '*PEAR/Installer.php',
         '*PEAR/Registry.php',
         '*PEAR/Remote.php',
+        '*PEAR/Start.php',
+        '*PEAR/Start/CLI.php',
         '*PEAR/Validate.php',
         '*PEAR/XMLParser.php',
-        'PEAR.php',
-        '*Archive/Tar.php',
-        '*Console/Getopt.php',
-        'System.php',
-    ));
+    ), false, $peardir);
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'PEAR.php', 'PEAR.php');
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'Archive/Tar.php', 'Archive/Tar.php');
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'Console/Getopt.php', 'Console/Getopt.php');
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'System.php', 'System.php');
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'OS/Guess.php', 'OS/Guess.php');
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'Structures_Graph/Structures/Graph.php', 'Structures/Graph.php');
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'Structures_Graph/Structures/Graph/Node.php', 'Structures/Graph/Node.php');
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'Structures_Graph/Structures/Graph/Manipulator/AcyclicTest.php', 'Structures/Graph/Manipulator/AcyclicTest.php');
+$creator->addFile($peardir . DIRECTORY_SEPARATOR . 'Structures_Graph/Structures/Graph/Manipulator/TopologicalSorter.php', 'Structures/Graph/Manipulator/TopologicalSorter.php');
 $creator->savePhar(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'install-pear-nozlib.phar');
 ?>
