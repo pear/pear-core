e08b631d0419d7f9a52bb02625b18f3e750ec66a
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-05-13 21:09:58 +0000

    kill E_STRICT/E_DEPRECATED, otherwise everything fails
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@259717 c90b9560-bf6c-de11-be94-00142212c4b1

--- PEAR/RunTest.php
+++ PEAR/RunTest.php
@@ -92,7 +92,13 @@ class PEAR_RunTest
      */
     function PEAR_RunTest($logger = null, $options = array())
     {
-        $this->ini_overwrites[] = 'error_reporting=' . E_ALL;
+        if (!defined('E_DEPRECATED')) {
+            define('E_DEPRECATED', 0);
+        }
+        if (!defined('E_STRICT')) {
+            define('E_STRICT', 0);
+        }
+        $this->ini_overwrites[] = 'error_reporting=' . E_ALL & ~E_DEPRECATED & ~E_STRICT;
         if (is_null($logger)) {
             require_once 'PEAR/Common.php';
             $logger = new PEAR_Common;
