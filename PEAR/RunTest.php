93b66d6b5d7e12c2db38741132c2fe6f4adb801b
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-05-14 02:30:16 +0000

    fix error_reporting default value
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@259727 c90b9560-bf6c-de11-be94-00142212c4b1

--- PEAR/RunTest.php
+++ PEAR/RunTest.php
@@ -98,7 +98,7 @@ class PEAR_RunTest
         if (!defined('E_STRICT')) {
             define('E_STRICT', 0);
         }
-        $this->ini_overwrites[] = 'error_reporting=' . E_ALL & ~E_DEPRECATED & ~E_STRICT;
+        $this->ini_overwrites[] = 'error_reporting=' . (E_ALL & ~(E_DEPRECATED | E_STRICT));
         if (is_null($logger)) {
             require_once 'PEAR/Common.php';
             $logger = new PEAR_Common;
