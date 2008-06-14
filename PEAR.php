524959b06e6a67d7b9e7cdabd1d421e00ecc17bc
dufuz <dufuz@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-06-14 08:47:34 +0000

    Add a bit hackish but working way of getting rid of the strict warning in the shutdown function that  wasn't being suppressed like it should. Those that want can use PEAR5::getStaticProperty  if you want a PHP 5 strict thingy for this
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@261173 c90b9560-bf6c-de11-be94-00142212c4b1

--- PEAR.php
+++ PEAR.php
@@ -768,6 +768,40 @@ class PEAR
     // }}}
 }
 
+if (PEAR_ZE2) {
+    /**
+     * This is only meant for PHP 5 to get rid of certain strict warning
+     * that doesn't get hidden since it's in the shutdown function
+     */
+    class PEAR5
+    {
+        /**
+        * If you have a class that's mostly/entirely static, and you need static
+        * properties, you can use this method to simulate them. Eg. in your method(s)
+        * do this: $myVar = PEAR5::getStaticProperty('myclass', 'myVar');
+        *
+        * @access public
+        * @param  string $class  The calling classname, to prevent clashes
+        * @param  string $var    The variable to retrieve.
+        * @return mixed   A reference to the variable. If not set it will be
+        *                 auto initialised to NULL.
+        */
+        static function getStaticProperty($class, $var)
+        {
+            static $properties;
+            if (!isset($properties[$class])) {
+                $properties[$class] = array();
+            }
+
+            if (!array_key_exists($var, $properties[$class])) {
+                $properties[$class][$var] = null;
+            }
+
+            return $properties[$class][$var];
+        }
+    }
+}
+
 // {{{ _PEAR_call_destructors()
 
 function _PEAR_call_destructors()
@@ -777,9 +811,16 @@ function _PEAR_call_destructors()
         sizeof($_PEAR_destructor_object_list))
     {
         reset($_PEAR_destructor_object_list);
-        if (PEAR::getStaticProperty('PEAR', 'destructlifo')) {
+        if (PEAR_ZE2) {
+            $destructLifoExists = PEAR5::getStaticProperty('PEAR', 'destructlifo');
+        } else {
+            $destructLifoExists = PEAR::getStaticProperty('PEAR', 'destructlifo');
+        }
+
+        if ($destructLifoExists) {
             $_PEAR_destructor_object_list = array_reverse($_PEAR_destructor_object_list);
         }
+
         while (list($k, $objref) = each($_PEAR_destructor_object_list)) {
             $classname = get_class($objref);
             while ($classname) {
