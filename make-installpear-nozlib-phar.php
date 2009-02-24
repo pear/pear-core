1ef51a424a722e44b442e8ee1d3080516fc3b546
dufuz <dufuz@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-02-24 23:53:11 +0000

    use the bsd license
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@276387 c90b9560-bf6c-de11-be94-00142212c4b1

--- make-installpear-nozlib-phar.php
+++ make-installpear-nozlib-phar.php
@@ -15,12 +15,7 @@
  * </pre>
  *
  * finally, run this script using PHP 5.1's cli php
- *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+
  *
  * @category   pear
  * @package    PEAR
@@ -87,6 +82,7 @@ foreach ($packages as $name => $package) {
 }
 $install_files .= ');';
 echo "install_files is $install_files";
+
 $commandcontents = file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'install-pear.php');
 $commandcontents = str_replace(
     array(
