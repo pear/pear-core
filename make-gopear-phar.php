eae8c1007a034bf9be59d5e22704e6b4dc923fbb
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2009-02-24 23:53:11 +0000

    use the bsd license

--- make-gopear-phar.php
+++ make-gopear-phar.php
@@ -19,12 +19,7 @@
  * Next, check out pear/Console_Getopt and copy it into pear-core/
  * 
  * finally, run this script using PHP 5.1's cli php
- *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+
  *
  * @category   pear
  * @package    PEAR
