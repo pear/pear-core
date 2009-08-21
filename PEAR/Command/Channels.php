3499feed5c485a1c2fcfe0d2c83f3f280b5edc87
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2009-08-21 22:42:58 +0000

    Make sure people can't put - as the first character in the alias name as that throws off Console_Getopt

--- PEAR/Command/Channels.php
+++ PEAR/Command/Channels.php
@@ -674,7 +674,7 @@ configuration.',
             return $this->raiseError('No channel alias specified');
         }
 
-        if (count($params) !== 2) {
+        if (count($params) !== 2 || (!empty($params[1]) && $params[1]{0} == '-')) {
             return $this->raiseError(
                 'Invalid format, correct is: channel-alias channel alias');
         }
