de9217c76c192f39fc371e24bebc6d478714952b
dufuz <dufuz@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-03-28 23:54:22 +0000

    Fix Bug #16644 pear.bat returns syntax error when parenthesis are in install path. [dufuz] [patch by bwaters (Bryan Waters)]
    git-svn-id: http://svn.php.net/repository/pear/pear-core/pear/pear-core/trunk@297030 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/peardev.bat
+++ scripts/peardev.bat
@@ -5,10 +5,10 @@ REM PHP version 5
 REM ----------------------------------------------------------------------
 REM Copyright (c) 1997-2004 The PHP Group
 REM ----------------------------------------------------------------------
-REM  This source file is subject to version 3.0 of the PHP license, 
+REM  This source file is subject to version 3.0 of the PHP license,
 REM  that is bundled with this package in the file LICENSE, and is
 REM  available at through the world-wide-web at
-REM  http://www.php.net/license/3_0.txt. 
+REM  http://www.php.net/license/3_0.txt.
 REM  If you did not receive a copy of the PHP license and are unable to
 REM  obtain it through the world-wide-web, please send a note to
 REM  license@php.net so we can mail you a copy immediately.
@@ -110,6 +110,6 @@ ECHO The current value is:
 ECHO %PHP_PEAR_PHP_BIN%
 GOTO END
 :RUN
-"%PHP_PEAR_PHP_BIN%" -C -d memory_limit="-1" -d safe_mode=0 -d register_argc_argv="On" -d auto_prepend_file="" -d auto_append_file="" -d variables_order=EGPCS -d open_basedir="" -d output_buffering=1 -d include_path="%PHP_PEAR_INSTALL_DIR%" -f "%PHP_PEAR_INSTALL_DIR%\pearcmd.php" -- %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%PHP_PEAR_PHP_BIN%" -C -d memory_limit="-1" -d safe_mode=0 -d register_argc_argv="On" -d auto_prepend_file="" -d auto_append_file="" -d variables_order=EGPCS -d open_basedir="" -d output_buffering=1 -d "include_path='%PHP_PEAR_INSTALL_DIR%'" -f "%PHP_PEAR_INSTALL_DIR%\pearcmd.php" -- %1 %2 %3 %4 %5 %6 %7 %8 %9
 :END
 @ECHO ON
