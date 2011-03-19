fc297474c0050f3e75f0768e7e0d9f9c86ed3dcd
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2011-03-19 17:57:53 +0000

    Fixed Bug #18364: date.timezone errors for sh/bat files when TZ is not set in php.ini [dufuz]

--- scripts/pecl.sh
+++ scripts/pecl.sh
@@ -5,7 +5,7 @@ if test "x$PHP_PEAR_PHP_BIN" != "x"; then
   PHP="$PHP_PEAR_PHP_BIN"
 else
   if test "@php_bin@" = '@'php_bin'@'; then
-    PHP=php 
+    PHP=php
   else
     PHP="@php_bin@"
   fi
@@ -18,11 +18,11 @@ if test "x$PHP_PEAR_INSTALL_DIR" != "x"; then
 else
   if test "@php_dir@" = '@'php_dir'@'; then
     INCDIR=`dirname $0`
-    INCARG=""  
+    INCARG=""
   else
     INCDIR="@php_dir@"
     INCARG="-d include_path=@php_dir@"
   fi
 fi
 
-exec $PHP -C -n -q $INCARG -d output_buffering=1 -d variables_order=EGPCS -d safe_mode=0 -d register_argc_argv="On" $INCDIR/peclcmd.php "$@"
+exec $PHP -C -n -q $INCARG -d date.timezone=UTC -d output_buffering=1 -d variables_order=EGPCS -d safe_mode=0 -d register_argc_argv="On" $INCDIR/peclcmd.php "$@"
