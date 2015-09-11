0f255b531233736ca184ff24eb5957f519f2fcef
Remi Collet <fedora@famillecollet.com>
2015-09-11 07:12:44 +0200

    Fix pear issue 20946
    Warning: Declaration of PEAR_Builder::log($level, $msg) should be compatible
    with PEAR_Common::log($level, $msg, $append_crlf = true)

--- PEAR/Builder.php
+++ PEAR/Builder.php
@@ -475,7 +475,7 @@ class PEAR_Builder extends PEAR_Common
         return ($exitcode == 0);
     }
 
-    function log($level, $msg)
+    function log($level, $msg, $append_crlf = true)
     {
         if ($this->current_callback) {
             if ($this->debug >= $level) {
@@ -483,6 +483,6 @@ class PEAR_Builder extends PEAR_Common
             }
             return;
         }
-        return parent::log($level, $msg);
+        return parent::log($level, $msg, $append_crlf);
     }
 }
