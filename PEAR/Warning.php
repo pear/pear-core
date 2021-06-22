b088e1b0c162307c3ef72d64c47469522ca86475
Remi Collet <remi@remirepo.net>
2021-06-22 08:18:38 +0200

    update PHP License to 3.01

--- PEAR/Warning.php
+++ PEAR/Warning.php
@@ -4,10 +4,10 @@
 // +----------------------------------------------------------------------+
 // | Copyright (c) 2004 The PEAR Group                                    |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
+// | This source file is subject to version 3.01 of the PHP license,      |
 // | that is bundled with this package in the file LICENSE, and is        |
 // | available at through the world-wide-web at                           |
-// | http://www.php.net/license/3_0.txt.                                  |
+// | https://www.php.net/license/3_01.txt                                 |
 // | If you did not receive a copy of the PHP license and are unable to   |
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
--- scripts/pecl.bat
+++ scripts/pecl.bat
@@ -5,10 +5,10 @@ REM PHP version 5
 REM ----------------------------------------------------------------------
 REM Copyright (c) 1997-2004 The PHP Group
 REM ----------------------------------------------------------------------
-REM  This source file is subject to version 3.0 of the PHP license,
+REM  This source file is subject to version 3.01 of the PHP license,
 REM  that is bundled with this package in the file LICENSE, and is
 REM  available at through the world-wide-web at
-REM  http://www.php.net/license/3_0.txt.
+REM  https://www.php.net/license/3_01.txt
 REM  If you did not receive a copy of the PHP license and are unable to
 REM  obtain it through the world-wide-web, please send a note to
 REM  license@php.net so we can mail you a copy immediately.
