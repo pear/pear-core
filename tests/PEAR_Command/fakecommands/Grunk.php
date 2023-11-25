79d912cb2cea0524946b8e59c4ca30c1e35f0f65
Chuck Burgess <ashnazg@php.net>
2023-11-24 18:30:56 -0600

    fix lingering license references to PHP license

--- tests/PEAR_Command/fakecommands/Grunk.php
+++ tests/PEAR_Command/fakecommands/Grunk.php
@@ -1,21 +1,15 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 5                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author: Stig Bakken <ssb@php.net>                                    |
-// +----------------------------------------------------------------------+
-//
+/**
+ * PEAR, the PHP Extension and Application Repository
+ *
+ * PHP versions 4 and 5
+ *
+ * @category   pear
+ * @package    PEAR
+ * @author     Stig Bakken <ssb@php.net>
+ * @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ * @link       http://pear.php.net/package/PEAR
+ */
 
 require_once "PEAR/Command/Common.php";
 
--- tests/PEAR_Downloader/test_upgrade_pecl/sqlite.c
+++ tests/PEAR_Downloader/test_upgrade_pecl/sqlite.c
@@ -4,20 +4,18 @@
    +----------------------------------------------------------------------+
    | Copyright (c) 1997-2003 The PHP Group                                |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 3.0 of the PHP license,       |
+   | This source file is subject to the New BSD License,                  |
    | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_0.txt.                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
+   | available at through the world-wide-web at                           |
+   | http://opensource.org/licenses/bsd-license.php.                      |
+   | If you did not receive a copy of the New BSD License and are unable  |
+   | to obtain it through the world-wide-web, please send a note to       |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
    | Authors: Wez Furlong <wez@thebrainroom.com>                          |
    |          Tal Peer <tal@php.net>                                      |
    |          Marcus Boerger <helly@php.net>                              |
    +----------------------------------------------------------------------+
-
-   $Id$ 
 */
 
 #ifdef HAVE_CONFIG_H
